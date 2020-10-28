0d8b6f20dd34830b04802960181ab4839fec9a10
kojiro.ito <k.mars0605@gmail.com>
2020-10-28 18:39:57 +0900

    Initial Commit

---
+++ privacy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  privacy
+//
+//  Created by 伊藤光次郎 on 2020/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
